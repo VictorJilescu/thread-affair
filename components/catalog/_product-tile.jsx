d4d794a5814de91966df11cdb31d0df47aff0375
VictorJilescu <97190875+VictorJilescu@users.noreply.github.com>
2022-07-23 13:59:38 +0300

    Carousel thread affair

--- components/catalog/_product-tile.jsx
+++ components/catalog/_product-tile.jsx
@@ -1,13 +1,12 @@
-export default function ProductTile() {
+export default function ProductTile({ product = {} }) {
+  const { productPrice = '', productName = '', productImage = '' } = product;
+
   return (
     <article className="product-tile">
       <header className="product-tile-header">
         <div className="product-tile-image">
-          <a href="" title="Thread Affair Gingham Suit">
-            <img
-              src="/images/products/product-01a.jpg"
-              alt="Thread Affair Gingham Suit"
-            />
+          <a href="" title={productName}>
+            <img src={productImage} alt={productName} />
           </a>
         </div>
 
@@ -17,15 +16,14 @@ export default function ProductTile() {
 
         <div className="product-tile-headings">
           <h1>
-            <a href="" className="Thread Affair Gingham Suit">
-              <span>Thread Affair</span> <span>Ghingham</span>
-              Suit
+            <a href="" className={productName}>
+              {productName}
             </a>
           </h1>
 
           <h2>
-            <a href="" title="Thread Affair Gingham Suit">
-              Berlin fit-Regular
+            <a href="" title={productName}>
+              {productName}
             </a>
           </h2>
         </div>
@@ -35,7 +33,7 @@ export default function ProductTile() {
         <div className="product-tile-pricing">
           <span className="product-price-regular">399</span>
           <span className="product-price">
-            USD <span>299</span>
+            USD <span>{productPrice}</span>
           </span>
         </div>
       </section>
